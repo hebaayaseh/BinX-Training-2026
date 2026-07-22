2b8242fe7c33bc1158408b38d1d02c2f346013c0
hebaayase <hebaheshamayase@gmail.com>
2026-07-22 17:17:40 +0300

    Edit Day 4

--- Week1/Day4/LINQ/LINQ/obj/Debug/net8.0/LINQ.AssemblyInfo.cs
+++ Week1/Day4/LINQ/LINQ/obj/Debug/net8.0/LINQ.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2464a7a1cd8c0989d4e96182a935fb3839e7a9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c9bb838e4b12431ae4c19ab3cf9afb1d8aca169")]
 [assembly: System.Reflection.AssemblyProductAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
