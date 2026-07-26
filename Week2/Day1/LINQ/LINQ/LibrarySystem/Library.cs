4431b52f9c60e6753db5f337553cf23a12d3eb44
hebaayase <hebaheshamayase@gmail.com>
2026-07-26 17:33:07 +0300

    Day 1 at Week 2

---
+++ Week2/Day1/LINQ/LINQ/LibrarySystem/Library.cs
@@ -0,0 +1,71 @@
+﻿using LINQ.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ.LibrarySystem
+{
+    public class Library
+    {
+        private List<Book> books = new List<Book>();
+        private List<User> users = new List<User>();
+
+        public async Task Register(string email, string password)
+        {
+            foreach (var user in users)
+            {
+                if (user.getEmail == email)
+                {
+                    await Task.Delay(3000);
+                    Console.WriteLine("email already exist");
+
+                }
+            }
+            users.Add(new User(email, password));
+            Console.WriteLine("Account Crated Successfuly");
+        }
+
+        public bool login(string email, string password)
+        {
+            foreach (var user in users)
+            {
+                if (user.getEmail == email && user.getPassword == password)
+                {
+                    Console.WriteLine("Login Successfuly");
+                    return true;
+                }
+            }
+            Console.WriteLine("Invalid Email Or Password!");
+            return false;
+        }
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+            Console.WriteLine("Book Added Succssfuly");
+        }
+
+        public void RemoveBook(int Id)
+        {
+            books.RemoveAll(b => b.getId == Id);
+            Console.WriteLine("Book Removed Succssfuly");
+        }
+
+        public void ShowBooks()
+        {
+            int count = 1;
+            foreach (var book in books)
+            {
+                Console.WriteLine($"{count} : {book.getId} {book.getTitle}");
+                count++;
+            }
+        }
+        // polymorphism 
+        public void Borrow(IBorrowable borrow)
+        {
+            borrow.Borrow();
+        }
+
+    }
+}
---
+++ Week2/Day1/LINQ/LINQ/Repository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem
+{
+    // T must be a reference type (class) dont accept int or string ... 
+    // This repositry works at any class even different class 
+    public class Repository<T> where T : class
+    {
+        private readonly List<T> item = new List<T>();
+        // Add :
+        public void Add(T item)
+        {
+            this.item.Add(item);
+        }
+
+        // GetAll :
+        public List<T> GetAll()
+        {
+            return item;
+        }
+
+        // IReadOnlyList :
+        public IReadOnlyList<T> ReadOnlyList()
+        {
+            return item.AsReadOnly();
+        }
+
+        // Find :
+        public List<T> Find(Func<T,bool> itemes)
+        {
+            return item.Where(itemes).ToList();
+        }
+
+        //  Find method taking a predicate :
+        public T? FindPredicate(Predicate<T> predicate)
+        {
+            return item.Find(predicate);
+        }
+
+
+    }
+}
