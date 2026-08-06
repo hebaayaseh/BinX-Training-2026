823579a175aa6b420889d3a55d32c3930af64e5a
hebaayase <hebaheshamayase@gmail.com>
2026-08-06 20:21:57 +0300

    Day 4 Week 3

---
+++ Week3/Day4/LibraryManagment/LibraryManagment/Interface/IUpdateCategory.cs
@@ -0,0 +1,9 @@
+﻿using LibraryManagment.DTO;
+
+namespace LibraryManagment.Interface
+{
+    public interface IUpdateCategory
+    {
+        Task<UpdateCategoryResponseDto> UpdateCategory(int id, UpdateCategoryRequestDto request);
+    }
+}
