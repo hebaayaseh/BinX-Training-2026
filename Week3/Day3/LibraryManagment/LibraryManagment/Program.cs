8286d07ef72dfab1577ae87b55234ebb5ff2a9ac
hebaayase <hebaheshamayase@gmail.com>
2026-08-06 15:10:04 +0300

    Day 3 Week 3

---
+++ Week3/Day3/LibraryManagment/LibraryManagment/Program.cs
@@ -0,0 +1,43 @@
+
+using LibraryManagment.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var connectionString = builder.Configuration.GetConnectionString("Connection");
+            builder.Services.AddDbContext<AppDBContext>(options =>
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
