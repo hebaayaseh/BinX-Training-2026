639dce4fd9703ad248bbacf1e2a570e1e353a2ee
hebaayase <hebaheshamayase@gmail.com>
2026-08-11 01:37:05 +0300

    Addition Models at database

--- Project/CardioTrack/CardioTrack/Data/CardioTraackDbContext.cs
+++ Project/CardioTrack/CardioTrack/Data/CardioTraackDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CardioTrack.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CardioTrack.Data
 {
@@ -6,6 +7,16 @@ namespace CardioTrack.Data
     {
         public CardioTraackDbContext(DbContextOptions<CardioTraackDbContext> options)
         : base(options) { }
+
+        public DbSet<User> users=> Set<User>();
+        public DbSet<Patient> patients => Set<Patient>();
+        public DbSet<VitalSign> vitalSigns => Set<VitalSign>();
+        public DbSet<VitalSignAlert> vitalSignAlerts => Set<VitalSignAlert>();
+        public DbSet<MedicalHistory> medicalHistories => Set<MedicalHistory>();
+        public DbSet<Medication> medications => Set<Medication>();
+        public DbSet<Appointment> appointments => Set<Appointment>();
+
+
 
     }
 }
